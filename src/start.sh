fb75ebb61b4fd6c2818d17cc318bdff321701cc3
Nanwan <2485332377@qq.com>
2025-08-02 03:32:33 +0800

    添加了一键起飞的脚本

---
+++ src/start.sh
@@ -0,0 +1,6 @@
+source ~/.bashrc
+gnome-terminal --window -e 'bash -c "source devel/setup.bash && roslaunch livox_ros_driver2 msg_MID360.launch; exec bash"' \
+--tab -e 'bash -c "sleep 4; source devel/setup.bash && roslaunch fast_lio mapping_mid360.launch; exec bash"' \
+--tab -e 'bash -c "sleep 4; source devel/setup.bash && roslaunch mavros px4.launch; exec bash"' \
+--tab -e 'bash -c "sleep 4; source devel/setup.bash && roslaunch ground_station_comm ground_station.launch; exec bash"' \
+--tab -e 'bash -c "sleep 4; source devel/setup.bash && roslaunch yolo11_ros yolo11_detection.launch; exec bash"' \
